8c79146228a901254fba0e18cfca36eb2400c1bb
Alex Marin Mendez <alexmarinmendez@gmail.com>
2024-11-13 10:02:42 -0500

    Agregar archivos de proyecto.

---
+++ WebApplication1/Controllers/CustomersController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Dao;
+
+namespace WebApplication1.Controllers
+{
+    public class CustomersController : Controller
+    {
+        CustomersDao customersDao = new CustomersDao();
+        // GET: Customers
+        public ActionResult ListadoCustomers()
+        {
+            var listado = customersDao.ListarCustomers();
+            return View(listado); 
+        }
+    }
+}
---
+++ WebApplication1/Dao/CustomersDao.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using WebApplication1.Models;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace WebApplication1.Dao
+{
+    public class CustomersDao
+    {
+        string cadena = ConfigurationManager.ConnectionStrings["sqlConn"].ConnectionString;
+
+        public List<Customers> ListarCustomers()
+        {
+            SqlConnection conn = new SqlConnection(cadena);
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("SELECT * FROM Customers", conn);
+            cmd.CommandType = CommandType.Text;
+            SqlDataReader reader = cmd.ExecuteReader();
+            List<Customers> lista = new List<Customers>();
+            Customers obj;
+            while (reader.Read())
+            {
+                obj = new Customers()
+                {
+                    IdCliente = reader.GetString(0),
+                    NombreCliente = reader.GetString(1),
+                    Direccion = reader.GetString(2),
+                    Telefono = reader.GetString(3),
+                    Pais = reader.GetString(4),
+                };
+                lista.Add(obj);
+            }
+            reader.Close();
+            conn.Close();
+            return lista;
+        }
+    }
+}
